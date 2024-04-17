ab90c7a102e48b38d9b68e482d7b3169ca374135
yxsj245 <107445049+yxsj245@users.noreply.github.com>
2024-04-17 10:49:46 +0800

    Update run.py

--- run.py
+++ run.py
@@ -28,12 +28,10 @@ def download_file(url, directory, filename):
     # 发送HTTP请求并获取响应
     response = requests.get(url, headers=headers, stream=True)
     if response.status_code == 200:
-        # 创建文件夹
-        os.makedirs(directory, exist_ok=True)
-        # 拼接文件保存路径
-        filepath = os.path.join(directory, filename)
         # 获取文件大小
         total_size = int(response.headers.get('content-length', 0))
+        # 拼接文件保存路径
+        filepath = os.path.join(directory, filename)
         # 下载文件并显示进度条
         with open(filepath, 'wb') as f:
             with tqdm(total=total_size, unit='B', unit_scale=True, unit_divisor=1024) as progress_bar:
@@ -73,8 +71,8 @@ def get_first_attachment_link(url):
             redirected_url = get_redirected_url(file_url)
             if redirected_url:
                 title = attachment_link.get('title')
-                print("Title:", title)
-                print("Href:", redirected_url)
+                # print("Title:", title)
+                # print("Href:", redirected_url)
                 return title, redirected_url
             else:
                 print(f"Failed to get redirected URL for {file_url}")
@@ -87,28 +85,50 @@ def get_first_attachment_link(url):
 
 #下载核心文件
 def download_and_extract_attachment(url):
-    print("正在访问和解析网站，如果长时间无反应后崩溃则是网络或网站问题")
+    print("INFO:正在访问和解析网站，如果长时间无反应后崩溃则是网络或网站问题")
     title, first_attachment_url = get_first_attachment_link(url)
     if first_attachment_url:
         filename = os.path.basename(first_attachment_url)
-        print(f"成功解析出下载地址，文件名称: {filename}")
+        print(f"INFO:成功解析出下载地址，文件名称: {filename}")
         # 解析URL获取最后一个路径部分作为文件夹名称
         folder_name = unquote(urlsplit(url).path.split('/')[-2])
         # 创建以标题为名的文件夹
         folder_path = os.path.join('data', folder_name)
+        # 创建文件夹
+        os.makedirs(folder_path, exist_ok=True)
         # 获取文件路径
         filepath = os.path.join(folder_path, filename)
+        #文件名称变量
+        directory_path = os.path.dirname(filepath)
+
+        files = os.listdir(directory_path)
+        if not files:
+            None
+        else:
+            for file in files:
+                xuanze = input('interaction:检测到修改器已下载过，是否删除之前下载的(y表示确认删除,输入任意内容表示不删除)')
+                if xuanze == 'y':
+                    files = os.listdir(directory_path)
+                    files = files[0]
+                    # print(files)
+                    file_path = os.path.join(directory_path, files)
+                    # 删除文件
+                    os.remove(file_path)
+                    print("INFO:旧版本文件已删除")
+                else:
+                    print('WARN:用户已取消删除，这可能会导致使用此程序启动修改器为之前下载的')
+
         # 下载文件
-        print(f"开始下载修改器")
+        print(f"INFO:开始下载修改器")
         if not os.path.exists(folder_path) or filename not in os.listdir(folder_path):
             filepath = download_file(first_attachment_url, folder_path, filename)
             if filepath:
-                print("提取修改器主程序")
+                print("INFO:提取修改器主程序")
                 unzip_file(filepath, folder_path)
                 delete_file(filepath)
-                print("下载完毕，请重新运行程序即可启动修改器界面选择对应修改器即可启动")
+                print("INFO:下载完毕，请重新运行程序即可启动修改器界面选择对应修改器即可启动")
         else:
-            print("File already exists in target directory.")
+            print("ERROR:目标目录中已存在文件。可能是解压文件失败。")
 
         # 更新 JSON 文件中的值
         update_data_json(True)
@@ -148,7 +168,7 @@ def launch_exe(directory):
         exe_path = os.path.join(directory, exes[0])
         os.system('start "" "{}"'.format(exe_path))
     else:
-        print("No executable (.exe) files found in the directory.")
+        print("ERROR:在目录中找不到可执行文件(.exe);可能是您手动删除")
 
 
 # 检查是否首次运行程序
@@ -157,10 +177,10 @@ if not os.path.exists("data.json"):
 data = read_data_json()
 if data is not None and not data["run"]:
     # 调用函数并传入指定网站的URL
-    urldata = input("请输入需要下载的风灵月影修改器地址:")
+    urldata = input("interaction:请输入需要下载的风灵月影修改器地址:")
     url = urldata
     download_and_extract_attachment(url)
-    print("程序运行完毕，按任意键退出...")
+    print("INFO:程序运行完毕，按任意键退出...")
     input()
 else:
     #启动修改器主程序
@@ -170,12 +190,12 @@ else:
         print(f"{i}. {folder}")
 
     try:
-        print("权限提升安全提醒：由于Windows权限规则，启动exe文件必须以管理员身份运行，输入后程序将会请求Windows以管理员身份启动修改器。")
-        choice = int(input("请输入要启动的游戏名称修改器(输入前面序号即可): "))
+        print("WARN:权限提升安全提醒：由于Windows权限规则，启动exe文件必须以管理员身份运行，输入后程序将会请求Windows以管理员身份启动修改器。")
+        choice = int(input("interaction:请输入要启动的游戏名称修改器(输入前面序号即可): "))
         if 1 <= choice <= len(folders):
             selected_folder = os.path.join(data_dir, folders[choice - 1])
             launch_exe(selected_folder)
         else:
-            print("请输入正确序号")
+            print("ERROR:请输入正确序号")
     except ValueError:
-        print("捕捉到错误")
+        print("ERROR:捕捉到错误")
